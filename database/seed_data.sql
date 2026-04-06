6be2b46d05dddd371b468ecd1e7ea7ae7180279a
aikushkushal <kushkushal@gmail.com>
2026-04-05 22:05:51 -0500

    Create seed data file with initial ingredient data

---
+++ database/seed_data.sql
@@ -0,0 +1,34 @@
+-- Seed data for ingredients
+
+-- Vegetables
+INSERT INTO ingredients (name, type) VALUES
+  ('Carrot', 'Vegetable'),
+  ('Broccoli', 'Vegetable'),
+  ('Spinach', 'Vegetable'),
+  ('Potato', 'Vegetable');
+
+-- Fruits
+INSERT INTO ingredients (name, type) VALUES
+  ('Apple', 'Fruit'),
+  ('Banana', 'Fruit'),
+  ('Orange', 'Fruit'),
+  ('Strawberry', 'Fruit');
+
+-- Meat
+INSERT INTO ingredients (name, type) VALUES
+  ('Chicken', 'Meat'),
+  ('Beef', 'Meat'),
+  ('Pork', 'Meat'),
+  ('Fish', 'Meat');
+
+-- Dairy
+INSERT INTO ingredients (name, type) VALUES
+  ('Milk', 'Dairy'),
+  ('Cheese', 'Dairy'),
+  ('Yogurt', 'Dairy'),
+  ('Butter', 'Dairy');
+
+-- Eggs
+INSERT INTO ingredients (name, type) VALUES
+  ('Chicken Egg', 'Eggs'),
+  ('Duck Egg', 'Eggs');
